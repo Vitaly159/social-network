dcbaac14566872d70d4963dade44a06aca75d877
vital <159vvf@gmail.com>
2022-10-29 11:32:25 +0500

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Box } from "@material-ui/core";
 import SignIn from "./compoments/SignIn/SignIn";
 
 function App() {
   return (
-    <>
+    <Box>
       <SignIn />
-    </>
+    </Box>
   );
 }
 
